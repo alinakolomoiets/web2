ffeca6d160de5ed6e038daba402e715e2626371d
opilane <opilane@HP-CZC2349HT3>
2023-09-07 12:24:14 +0300

    Add project files.

---
+++ Controllers/ToodeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using web2.Models;
+
+namespace web2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToodeController : ControllerBase
+    {
+        private static Toode _toode = new Toode(1, "Koola", 1.5, true);
+
+        // GET: toode
+        [HttpGet]
+        public Toode GetToode()
+        {
+            return _toode;
+        }
+
+
+        // GET: toode/suurenda-hinda
+        [HttpGet("suurenda-hinda")]
+        public Toode SuurendaHinda()
+        {
+            _toode.Price = _toode.Price + 1;
+            return _toode;
+        }
+
+        // GET: toode/activtoode
+        [HttpGet("activtoode")]
+        public Toode ActivToode()
+        {
+           if (_toode.IsActive ==false)
+            {
+                _toode.IsActive = true;
+            }
+            else
+            {
+                _toode.IsActive = false;
+            }
+           return _toode;
+        }
+
+        // GET: toode/muuda-nimi
+        [HttpGet("muuda-nimi")]
+        public Toode MuudaNimi(string name)
+        {
+           _toode.Name = name;
+            return _toode;
+        }
+
+        // GET: toode/muuda-hind
+        [HttpGet("muuda-hind")]
+        public Toode MuudaHind(double korrutada)
+        {
+            _toode.Price *= korrutada;
+            return _toode;
+        }
+    }
+}
---
+++ Controllers/TootedController .cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using web2.Models;
+
+namespace web2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TootedController : ControllerBase
+    {
+        private static List<Toode> _tooted = new()
+        {
+            new Toode(1,"Koola", 1.5, true),
+            new Toode(2,"Fanta", 1.0, false),
+            new Toode(3,"Sprite", 1.7, true),
+            new Toode(4,"Vichy", 2.0, true),
+            new Toode(5,"Vitamin well", 2.5, true)
+        };
+
+        // GET https://localhost:4444/tooted
+        [HttpGet]
+        public List<Toode> Get()
+        {
+            return _tooted;
+        }
+
+        // DELETE https://localhost:4444/tooted/kustuta/0
+        [HttpDelete("kustuta/{index}")]
+        public List<Toode> Delete(int index)
+        {
+            _tooted.RemoveAt(index);
+            return _tooted;
+        }
+
+        [HttpDelete("kustuta2/{index}")]
+        public string Delete2(int index)
+        {
+            _tooted.RemoveAt(index);
+            return "Kustutatud!";
+        }
+
+        // POST https://localhost:4444/tooted/lisa/1/Coca/1.5/true
+        [HttpPost("lisa/{id}/{nimi}/{hind}/{aktiivne}")]
+        public List<Toode> Add(int id, string nimi, double hind, bool aktiivne)
+        {
+            Toode toode = new Toode(id, nimi, hind, aktiivne);
+            _tooted.Add(toode);
+            return _tooted;
+        }
+
+        [HttpPost("lisa2")]
+        public List<Toode> Add2(int id, string nimi, double hind, bool aktiivne)
+        {
+            Toode toode = new Toode(id, nimi, hind, aktiivne);
+            _tooted.Add(toode);
+            return _tooted;
+        }
+
+        // PATCH https://localhost:4444/tooted/hind-dollaritesse/1.5
+        [HttpPatch("hind-dollaritesse/{kurss}")]
+        public List<Toode> UpdatePrices(double kurss)
+        {
+            for (int i = 0; i < _tooted.Count; i++)
+            {
+                _tooted[i].Price = _tooted[i].Price * kurss;
+            }
+            return _tooted;
+        }
+    }
+}
